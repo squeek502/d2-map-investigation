ebe1fb689a8d002fc65ddbe5e1821e92d4d77f9b
Ryan Liptak <squeek502@hotmail.com>
2020-04-02 20:01:09 -0700

    Don't pass D2CLIENT_LoadAct function pointers to D2COMMON_LoadAct
    This might be a mistake but it seems okay. It gets rid of the automap flickering/the act 1 town getting duplicated across the automap/memory usage increasing over time when iterating seeds

--- BH/MapInvestigation.cpp
+++ BH/MapInvestigation.cpp
@@ -75,7 +75,9 @@ void InvestigateMaps()
 		int lastAct = 2; //(isLOD ? 5 : 4);
 		for (int act = 1; act <= lastAct; act++)
 		{
-			Act* pAct = D2COMMON_LoadAct(act - 1, mapSeed, isLOD, 0, D2CLIENT_GetDifficulty(), NULL, actStartingLevelIds[act - 1], D2CLIENT_LoadAct_1, D2CLIENT_LoadAct_2);
+			// the last two arguments seem to be related to automap updating; by setting them to NULL it stops some automap artifacts
+			// and gets rid of some memory-leak-like behavior (before D2CLIENT_LoadAct_1, D2CLIENT_LoadAct_2 were passed to these args)
+			Act* pAct = D2COMMON_LoadAct(act - 1, mapSeed, isLOD, 0, D2CLIENT_GetDifficulty(), NULL, actStartingLevelIds[act - 1], NULL, NULL);
 			if (!pAct || !pAct->pMisc) {
 				cout << "Failed to load act, bailing" << endl;
 				return;
